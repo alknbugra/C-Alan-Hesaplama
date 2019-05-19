08f2310c1b5d3af670054ee8e90d1f6c1109b082
Bugra Alkin <alknbugra@gmail.com>
2019-05-19 22:42:59 +0300

    Add files via upload

---
+++ ders3.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <locale.h>
+
+float alan_bul(){
+	float a;
+	float h;
+	
+	printf("Ücgenin tabanini giriniz : ");
+	scanf("%f",&a);
+	printf("Ücgenin yüksekligini giriniz : ");
+	getchar();
+	scanf("%f",&h);
+	return (a*h/2.0);
+}
+
+int main() {
+	setlocale(LC_ALL,"");
+	printf("\nÜcgenin alani %f dir.",alan_bul());
+	return 0;
+}
+
